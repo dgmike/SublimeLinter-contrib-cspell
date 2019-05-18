d5817db7736b27dca78009b51002ee6a6b3ab473
Michael <dgmike@users.noreply.github.com>
2019-05-18 18:47:02 -0300

    Update linter.py

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter, STREAM_STDOUT
 
 
 class CSpell(Linter):
-    cmd = 'cspell ${file_on_disk}'
+    cmd = 'cspell ${file} --no-color --no-summary'
     defaults = {'selector': 'source'}
     regex = r'^(?P<filename>[^:]*):(?P<line>\d+):(?P<col>\d+) - (?P<message>.*)$'
     tempfile_suffix = '-'
